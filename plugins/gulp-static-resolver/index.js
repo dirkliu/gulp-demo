182a4d05133b013bfcd3b589a5800676a4e7e8d8
dirkliu <liuqi0702@126.com>
2019-08-21 10:59:10 +0800

    构建优化！

--- plugins/gulp-static-resolver/index.js
+++ plugins/gulp-static-resolver/index.js
@@ -15,7 +15,7 @@ module.exports = function (versionFile) {
     var versionJson = JSON.parse(fs.readFileSync(versionFile, 'utf-8').replace(/[\n\t\r]/g, ''))
     var contents = file.contents.toString()
     Object.keys(versionJson).forEach(key => {
-      var staticReg = new RegExp('(?<=\\<script\\\s+[^\\\>]*src=[\\\'\\\"])' + key + '(?=[\\\'\\\"]\\\s*.*\\\>)', 'gm')
+      var staticReg = new RegExp('(?<=\\<\\w+\\s+[^\\>]*src=[\\\'\\\"])' + key + '(?=[\\\'\\\"]\\s*.*\\>)|(?<=\\<link\\s+[^\\>]*href=[\\\'\\\"])' + key + '(?=[\\\'\\\"]\\s*.*\\>)|(?<=\\burl\\([\\\"\\\']*)' + key + '(?=[\\\"\\\']*\\))', 'gm')
       contents = contents.replace(staticReg, key + '?' + versionJson[key])
     })
     file.contents = Buffer.from(contents)
